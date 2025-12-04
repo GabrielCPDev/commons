5a42a4f0f0fc741f8bcbe4b9bb7faace2579113c
GabrielCPDev <gabrielcpcontato@outlook.com>
2025-12-04 10:06:21 -0300

    corrigindo Alert.kt

--- src/main/kotlin/notifications/Alert.kt
+++ src/main/kotlin/notifications/Alert.kt
@@ -1,4 +1,26 @@
 package notifications
 
-class Alert {
+import models.ID
+import java.time.Instant
+
+abstract class Alert(
+    val id: ID,
+    val channels: List<NotificationChannel>,
+    val createdAt: Instant = Instant.now()
+) {
+    init {
+        require(channels.isNotEmpty()) {
+            "Channels cannot be empty"
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Alert) return false
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
